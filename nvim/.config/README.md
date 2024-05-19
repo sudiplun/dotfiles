001464c1f9ca0561f2b3db06e7f2fc42d058fbf0
Sudip Lun <sudeeplun@gmail.com>
2024-05-19 16:01:14 +0545

    remove nvim config

--- nvim/.config/README.md
+++
@@ -1,1 +0,0 @@
-Now i move to [AstroNvim](https://astronvim.com) && config is [AstroNvim-config](https://github.com/sudiplun/AstroNvim-Conf)
--- nvim/.config/init.lua
+++
@@ -1,8 +0,0 @@
-require("luniox.plugins-setup")
-require("luniox.core.options")
-require("luniox.core.keymaps")
-require("luniox.core.colorscheme")
-require("luniox.plugins.comment")
-require("luniox.plugins.nvim-tree")
-require("luniox.plugins.lualine")
-require("luniox.plugins.nvim-cmp")
