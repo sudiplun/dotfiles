b725c5c1af76de67de5a619a93f2de7ec38e40e7
sudiplun <sudeeplun@gmail.com>
2024-12-13 12:42:28 +0545

    delete

--- nvim/lua/plugins/tokyonight.lua
+++
@@ -1,8 +0,0 @@
-return { -- You can easily change to a different colorscheme.
-	"folke/tokyonight.nvim",
-	priority = 1000, -- Make sure to load this before all the other start plugins.
-	init = function()
-		vim.cmd.colorscheme("tokyonight-night")
-		--		vim.cmd.hi("Comment gui=none")
-	end,
-}
