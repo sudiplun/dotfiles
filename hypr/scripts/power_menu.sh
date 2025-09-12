40acabec3ff820efcad73b74c8e056d884aac533
Sudip Lun <sudeeplun@gmail.com>
2025-09-12 19:38:24 +0545

    fix gestures horizontal

--- hypr/scripts/power_menu.sh
+++ hypr/scripts/power_menu.sh
@@ -3,20 +3,20 @@
 # Function to show power menu
 show_power_menu() {
   local menu_options="\u200B  Lock
-\u200C󰤄  Sleep
-\u200D  Relaunch
-\u2060󰜉  Restart
-󰐥\u2063  Shutdown" # These first characters are invisible sort keys
+\u200C󰤄  sleep
+\u200D  relaunch
+\u2060󰜉  restart
+󰐥\u2063  shutdown" # These first characters are invisible sort keys
 
   local selection=$(echo -e "$menu_options" | wofi --show dmenu --prompt "Power Options" --width 200 --height 250 -O alphabetical)
 
   case "$selection" in
   *Lock*) hyprlock ;;
-  *Sleep*) systemctl suspend ;;
+  *sleep*) systemctl suspend ;;
   # *Relaunch*) hyprctl dispatch exit ;;
-  *Relaunch*) uwsm stop;;
-  *Restart*) systemctl reboot ;;
-  *Shutdown*) systemctl poweroff ;;
+  *relaunch*) uwsm stop;;
+  *restart*) systemctl reboot ;;
+  *shutdown*) systemctl poweroff ;;
   esac
 }
 
